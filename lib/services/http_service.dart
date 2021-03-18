a2d17b243231769430904afdfc53f571529c91df
AjjuSingh <ajaykumar.kumar900@gmail.com>
2021-03-18 13:03:24 +0530

    service: http service added

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -19,20 +19,60 @@ abstract class HttpService {
   String get userId => _userId!;
   set userId(String value) => _userId = value;
 
+  /////////////////////////////////////////////////////////
+  /// Dio http client setup
+  /// Base options and other stuff 👍
+  /////////////////////////////////////////////////////////
   late Response _response;
-  Dio dio = new Dio();
+  late Dio dio;
+  final String _baseUrl = "http://localhost:5000";
 
-  initInterceptors() {
-    dio.interceptors.add(InterceptorsWrapper(onRequest: (Options option) {
-      dio.interceptors.requestLock.lock();
-    }));
+  /// Init method to create instance of Dio
+  void init() {
+    // Base options
+    BaseOptions options = new BaseOptions(
+      baseUrl: "http://localhost:5000",
+    );
+    dio = new Dio(options);
   }
 
-  /// Define get, post, patch, update methods
-  Future<Response> get(
-      {String? endpoint,
-      Map<String, dynamic>? headers,
-      Map<String, dynamic>? queryParams}) async {
-    return _response;
+  /// Build headers
+  Map _buildHeaders({String? authToken}) {
+    Map<String, String> headers = Map();
+    headers["Content-Type"] = "application/json";
+    if (authToken != null) {
+      headers["Authorization"] = "Bearer $authToken";
+    }
+    return headers;
+  }
+
+  String _buildUrl({
+    required String endpoint,
+    Map<String, dynamic>? queryParams,
+  }) {
+    var apiUrl = Uri.parse(_baseUrl);
+    String apiPath = apiUrl.path + "/" + endpoint;
+    final url = Uri(
+            scheme: apiUrl.scheme,
+            host: apiUrl.host,
+            path: apiPath,
+            queryParameters: queryParams)
+        .toString();
+    return url;
+  }
+
+  Future<dynamic> get({
+    String? url,
+    Map<String, dynamic>? queryParams,
+  }) async {
+    try {
+      // TODO: Get user token from shared preference
+
+      // TODO: Pass token to _buildHeaders method
+
+      Response response =
+          await dio.get(url, queryParameters: queryParams, options: Options());
+      return response;
+    } catch (_) {}
   }
 }
